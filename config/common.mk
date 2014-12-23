9c6d6477a1499987f71d6c9a9fc9458a60d61b18
Janson Kang <temasek71@gmail.com>
2014-12-24 07:40:44 +0800

    Revert "Add SuperSU [2/2]"
    This reverts commit f76287e40a9909558af10c7f514823b9382c9e74.

--- config/common.mk
+++ config/common.mk
@@ -109,11 +109,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/init.d/99fstrim:system/etc/init.d/99fstrim
 
-# SuperSU
-PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
-    vendor/cm/prebuilt/common/etc/init.d/98SuperSUDaemon:system/etc/init.d/98SuperSUDaemon
-
 # Proprietary latinime lib needed for Keyboard swyping
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
